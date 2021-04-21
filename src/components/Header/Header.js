43ced2be12f1a851c76eb981cbc80487b0d72130
rglasgow-deloitte <rglasgow@deloitte.com>
2021-04-21 15:40:15 -0400

    Adds styling to dodIcon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import classes from "./Header.module.css";
 import { NavLink } from "react-router-dom";
 import dodLogo from "../../resources/internal/dodLogo.png";
+import Login from "./Login/Login";
 
 const Header = (props) => {
-  const [showLogin, setShowLogin] = useState(null);
+  const [showLogin, setShowLogin] = useState(false);
 
   // DS for generating buttons
   const buttons = [
@@ -13,11 +14,13 @@ const Header = (props) => {
     { name: "Tasks", route: "/tasks" },
   ];
 
-  const handleClick = () => {};
+  const handleClick = () => {
+    showLogin ? setShowLogin(Login) : setShowLogin(false);
+  };
 
-  const makeNavButtons = buttons.map((button) => {
+  const makeNavButtons = buttons.map((button, index) => {
     return (
-      <div className={classes.navItemWrapper}>
+      <div className={classes.navItemWrapper} key={index}>
         <NavLink exact to={button.route} className={classes.navItem}>
           {button.name}
         </NavLink>
@@ -26,19 +29,20 @@ const Header = (props) => {
   });
 
   return (
-    <header>
+    <header className={classes.root}>
       <div className={classes.imgBar}>
         <div className={classes.icon}>
           <img className={classes.logo} src={dodLogo} alt={""} />
           <div className={classes.logoText}>
-            <div>Experience Management Service</div>
-            <div>Digital Learning Portal</div>
-            <div>U.S Department of Defense</div>
+            <div className={classes.text}>Experience Management Service</div>
+            {/* <div>Digital Learning Portal</div> */}
+            <div className={(classes.text, classes.subText)}>
+              U.S Department of Defense
+            </div>
           </div>
         </div>
 
         <div className={classes.signIn}>Sign In</div>
-        
       </div>
       <div className={classes.nav}>{makeNavButtons}</div>
     </header>
