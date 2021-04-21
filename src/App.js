5abe5bdfb5175059a21a53a1f42771685f710c49
Swathi Chittlur <schittlur@deloitte.com>
2021-04-21 15:35:45 -0400

    Welcome screen

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import "./App.css";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import WelcomeScreen from "./components/Welcome Screen/WelcomeSceen"
 
 const App = () => {
   return (
     <div>
       <Router>
         <Header />
-        <div className={"content"}>
-          <Switch>
-            <Route path="/" component={null} />
-          </Switch>
+        <div className={"container"}>
+          <div className={"content"}>
+            <Switch>
+              <Route path="/" component={WelcomeScreen} />
+            </Switch>
+          </div>
         </div>
       </Router>
       <Footer />
