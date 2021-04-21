851c2b4f3726f701104f2498282ce8d042af7cb0
rglasgow-deloitte <rglasgow@deloitte.com>
2021-04-21 14:55:48 -0400

    Adds background to app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import "./App.css";
-import { Switch, Route, BrowserRouter as Router} from "react-router-dom";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <Router>
         <Header />
-        <Switch>
-          <Route path="/" component={null}/>
-        </Switch>
+        <div className={"content"}>
+          <Switch>
+            <Route path="/" component={null} />
+          </Switch>
+        </div>
       </Router>
       <Footer />
     </div>
