5871022ffa868c42861469989fd35a296ebf9baf
rglasgow-deloitte <rglasgow@deloitte.com>
2021-04-22 14:22:25 -0400

    Adds styling changes and line clamping

--- src/components/Catalogs/Catalogs.js
+++ src/components/Catalogs/Catalogs.js
@@ -4,33 +4,33 @@ import icon from "../../images/placeholder.jpg";
 
 const data = [
   {
-    title: "Catalog Title",
+    title: "Catalog Title 1",
+    content:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam voluptates doloribus numquam enim repellat vero minima quod quo vitae veniam? Ducimus animi ipsa reiciendis laborum distinctio adipisci deserunt cupiditate in? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam voluptates doloribus numquam enim repellat vero minima quod quo vitae veniam? Ducimus animi ipsa reiciendis laborum distinctio adipisci deserunt cupiditate in? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam voluptates doloribus numquam enim repellat vero minima quod quo vitae veniam? Ducimus animi ipsa reiciendis laborum distinctio adipisci deserunt cupiditate in? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam voluptates doloribus numquam enim repellat vero minima quod quo vitae veniam? Ducimus animi ipsa reiciendis laborum distinctio adipisci deserunt cupiditate in? lorem",
+  },
+
+  {
+    title: "Catalog Title 2",
     content:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam voluptates doloribus numquam enim repellat vero minima quod quo vitae veniam? Ducimus animi ipsa reiciendis laborum distinctio adipisci deserunt cupiditate in?",
   },
 
   {
-    title: "Catalog Title",
-    content:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam voluptates doloribus numquam enim repellat vero minima quod quo vitae veniam? Ducimus animi ipsa reiciendis laborum distinctio adipisci deserunt cupiditate in?",
-  },
-
-  {
-    title: "Catalog Title",
+    title: "Catalog Title 3",
     content:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam voluptates doloribus numquam enim repellat vero minima quod quo vitae veniam? Ducimus animi ipsa reiciendis laborum distinctio adipisci deserunt cupiditate in?",
   },
 ];
 
 const Catalog = ({ history }) => {
-  const handleClick = (e) => {
-    alert('You have clicked!!!!!!')
+  const handleClick = () => {
+    alert("You have clicked!!!!!!");
     // history.push("/");
   };
   const makeCatalogs = data.map((catalog, index) => {
     return (
       <div className={classes.catalog} key={index} onClick={handleClick}>
-        <img src={icon} alt="" />
+        <img src={icon} alt={'Alt img'}/>
         <div className={classes.contentWrapper}>
           <div className={classes.catalogTitle}>{catalog.title}</div>
           <div className={classes.content}>{catalog.content}</div>
