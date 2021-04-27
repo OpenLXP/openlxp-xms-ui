2d1ce01017b27578ac992f13379688aa33253bdd
rglasgow-deloitte <rglasgow@deloitte.com>
2021-04-27 15:38:44 -0400

    Refactors UI for catalog cars.

--- src/components/Catalogs/Catalogs.js
+++ src/components/Catalogs/Catalogs.js
@@ -1,56 +1,63 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 import classes from "./Catalogs.module.css";
 import icon from "../../images/placeholder.jpg";
 
-const data = ["Catalog Title 1", "Catalog Title 2", "Catalog Title 3"];
+const data = [
+  "Catalog Title 1",
+  "Catalog Title 2",
+  "Catalog Title 3",
+  "Catalog Title 4",
+  "Catalog Title 5",
+  "Catalog Title 6",
+  "Catalog Title 7",
+];
 
 const Catalog = (props) => {
   // Initial state for tracking catalogs
   const [catalogsState, setCatalogsState] = useState({
     providers: null,
     isLoading: false,
-    error: null
+    error: null,
   });
 
   const catalogs_api_url = process.env.REACT_APP_XIS_CATALOGS_API;
   let result = (
-    <div>
-      Error Loading experiences. Please contact an administrator.
-    </div>
-  )
+    <div>Error Loading experiences. Please contact an administrator.</div>
+  );
 
   /* Whenever the component first renders, make an API call to find providers
         using the keyword in the url */
   useEffect(() => {
     let url = catalogs_api_url;
-    setCatalogsState(previousState => {
+    setCatalogsState((previousState) => {
       const resultState = {
         providers: null,
         isLoading: true,
-        error: null
-      }
-      return resultState
+        error: null,
+      };
+      return resultState;
     });
-    axios.get(url)
-      .then(response => {
-        setCatalogsState(previousState => {
+    axios
+      .get(url)
+      .then((response) => {
+        setCatalogsState((previousState) => {
           return {
-            providers: response.data,
+            providers: response.data, // replace with arr
             isLoading: false,
-            error: null
-          }
+            error: null,
+          };
         });
       })
-      .catch(err => {
-        setCatalogsState(previousState => {
+      .catch((err) => {
+        setCatalogsState((previousState) => {
           return {
-            providers: null,
+            providers: data, // replace with arr
             isLoading: false,
-            error: err
-          }
-        })
+            error: err,
+          };
+        });
       });
   }, []);
 
@@ -59,12 +66,8 @@ const Catalog = (props) => {
   };
 
   if (catalogsState.isLoading) {
-    result = (
-      <div>
-        Loading...
-      </div>
-    )
-  } else if (catalogsState.providers && catalogsState.isLoading === false){
+    result = <div>Loading...</div>;
+  } else if (catalogsState.providers && catalogsState.isLoading === false) {
     result = catalogsState.providers.map((catalog, index) => {
       return (
         <div className={classes.catalog} key={index} onClick={handleClick}>
@@ -77,16 +80,20 @@ const Catalog = (props) => {
     });
   } else {
     result = (
-      <div>
-        Error Loading experiences. Please contact an administrator.
-      </div>
-    )
+      <div>Error Loading experiences. Please contact an administrator.</div>
+    );
   }
 
   return (
-    <div className={classes.container}>
-      <div className={classes.title}>{`Catalog (${data.length}`})</div>
-        {result}
+    <div>
+      <div className={classes.title}>
+        {`Catalog (${
+          /*Needs to count the number of results from the catalog call */
+          data.length
+        }`}
+        )
+      </div>
+      <div className={classes.container}>{result}</div>
     </div>
   );
 };
