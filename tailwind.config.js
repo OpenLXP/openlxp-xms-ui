a4441aa186354a00a8ebd52657df7c3fadd3171e
rglasgow-deloitte <rglasgow@deloitte.com>
2021-04-29 14:27:01 -0400

    Adds new colors to the styling.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      blue: {
+        light: "#09a3ce",
+        DEFAULT: "#0888aa",
+      },
+    },
     extend: {},
   },
   variants: {
