783dfe97a682fda98f8bf2eabcacdb5dccf5a47a
rglasgow-deloitte <rglasgow@deloitte.com>
2021-04-30 08:46:05 -0400

    Refactors code to extend the colors rather than overwrite them

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,14 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      blue: {
-        light: "#09a3ce",
-        DEFAULT: "#0888aa",
+    extend: {
+      colors: {
+        blue: {
+          light: "#09a3ce",
+          DEFAULT: "#0888aa",
+        },
       },
     },
-    extend: {},
   },
   variants: {
     extend: {},
