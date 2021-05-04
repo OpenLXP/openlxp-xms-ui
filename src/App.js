3ba6d1c6b8e9e578cea6993f97078c45f42c3671
rglasgow-deloitte <rglasgow@deloitte.com>
2021-05-04 10:59:09 -0400

    Refactors code to hide main page when logged in and show dashboard.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
-import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import {
+  Switch,
+  Route,
+  BrowserRouter as Router,
+  useLocation,
+} from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header/Header";
-import Login from "./components/Login/Login";
-import WelcomeScreen from "./components/WelcomeScreen/WelcomeScreen";
-import Catalog from "./components/Catalogs/Catalogs";
-import Courses from "./components/Courses/Courses";
-import Course from "./components/Course/Course";
-import Footer from "./components/Footer/Footer";
-import Breadcrumbs from "./components/Breadcrumbs/Breadcrumbs";
+import MainPage from "./pages/MainPage";
+import DashboardPage from "./pages/DashboardPage";
 
 const App = () => {
+  // Conditionally renders either the main website or the dashboard without
+  // the header or footer.
+  const loadContent = () => {
+    const location = useLocation();
+
+    const dashboard = () => (
+      <Route path="/dashboard" component={DashboardPage} />
+    );
+
+    const mainContent = () => <Route path="/" component={MainPage} />;
+
+    return location.pathname.includes("dashboard")
+      ? dashboard()
+      : mainContent();
+  };
+
   return (
-    <div className="flex flex-col min-h-screen">
-      <Router>
-        <Header />
-        <div className="w-10/12 p-2 mx-auto">
-          <div className="bg-gray-100 p-8 my-10 rounded-md">
-            <Switch>
-              <Route path="/" exact component={WelcomeScreen} />
-              <Route path="/catalogs" component={Catalog} />
-              <Route path="/courses" component={Courses} />
-              <Route path="/login" component={Login} />
-              <Route path="/course" component={Course} />
-            </Switch>
-          </div>
-        </div>
-      </Router>
-      <Footer />
-    </div>
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Route to="/" component={loadContent} />
+      </div>
+    </Router>
   );
 };
 
