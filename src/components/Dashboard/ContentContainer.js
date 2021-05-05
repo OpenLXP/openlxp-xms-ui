fc1558c2364f87b96bdf17a643112f92913df34c
rglasgow-deloitte <rglasgow@deloitte.com>
2021-05-05 14:08:54 -0400

    Reafactors route builder to render the component with optional 'exact' value.

--- src/components/Dashboard/ContentContainer.js
+++ src/components/Dashboard/ContentContainer.js
@@ -1,9 +1,12 @@
 import { Route, Switch } from "react-router";
 import Catalogs from "./Catalogs/Catalogs";
+import Courses from "./Courses/Courses";
 
 const ContentContainer = (props) => {
+  // Routes must be added in reverse order
   const pathToComponents = [
-    { path: "/dashboard/", component: Catalogs },
+    { path: "/dashboard/", component: Catalogs, isExact: true },
+    { path: "/dashboard/courses", component: Courses, isExact: false },
   ];
 
   return (
@@ -14,7 +17,11 @@ const ContentContainer = (props) => {
           <div className="py-4">
             <Switch>
               {pathToComponents.map((route) => (
-                <Route path={route.path} component={route.component} />
+                <Route
+                  path={route.path}
+                  component={route.component}
+                  exact={route.isExact}
+                />
               ))}
             </Switch>
           </div>
