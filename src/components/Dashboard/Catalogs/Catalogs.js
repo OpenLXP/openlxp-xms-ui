c8163b8b424139b04bccc95bd2766cbc10438c46
rglasgow-deloitte <rglasgow@deloitte.com>
2021-05-10 12:21:25 -0400

    Updates padding on dashboard content.

--- src/components/Dashboard/Catalogs/Catalogs.js
+++ src/components/Dashboard/Catalogs/Catalogs.js
@@ -62,8 +62,8 @@ const Catalogs = (props) => {
 
   return (
     <div>
-      <h1 className="font-sans text-2xl py-8 px-2">Course Catalogs</h1>
-      <div className="flex flex-wrap justify-around">{content}</div>
+      <h1 className="font-sans text-2xl py-8">Course Catalogs</h1>
+      <div className="flex flex-wrap justify-between">{content}</div>
     </div>
   );
 };
