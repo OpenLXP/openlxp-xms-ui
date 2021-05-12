1cc5a9dba4cd8f6f994ec4c16b2906a202e690e2
rglasgow-deloitte <rglasgow@deloitte.com>
2021-05-12 16:45:22 -0400

    Refactors the CourseRow.js to have a seperation of concerns. Additonally, adds a error handling if there is no data passed to the component.

--- src/components/Dashboard/Courses/CourseRow/CourseRow.js
+++ src/components/Dashboard/Courses/CourseRow/CourseRow.js
@@ -1,32 +1,8 @@
 import { NavLink } from "react-router-dom";
 
 const CourseRow = (props) => {
-  const data = props.data;
-  console.log(data);
+  const data = props.data || [];
 
-  const tableHeader = (
-    <tr>
-      <th
-        scope="col"
-        className="px-6 py-3 text-left text-gray-500 uppercase tracking-wider"
-      >
-        <div className="text-sm">Title</div>
-        <div className="text-xs font-thin">Course Code</div>
-      </th>
-
-      <th
-        scope="col"
-        className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap"
-      >
-        Course Status
-      </th>
-
-      <th
-        scope="col"
-        className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
-      />
-    </tr>
-  );
   const courseRows = data.map((row, index) => (
     <tr
       key={data.unique_record_identifier}
@@ -34,8 +10,7 @@ const CourseRow = (props) => {
         index % 2 === 0
           ? "bg-gray-50 hover:shadow-sm"
           : "bg-white hover:shadow-lg"
-      }
-    >
+      }>
       <td className="px-6 py-4 text-sm text-gray-900">
         <div className="font-medium">{row.metadata.Course.CourseTitle}</div>
         <div className="font-light">CC: {row.unique_record_identifier}</div>
@@ -50,25 +25,14 @@ const CourseRow = (props) => {
       <td className="px-6 py-4  text-center text-sm font-medium">
         <NavLink
           to={{ pathname: "/dashboard/course", state: row }}
-          className="text-blue hover:text-blue-light"
-        >
+          className="text-blue hover:text-blue-light">
           View
         </NavLink>
       </td>
     </tr>
   ));
 
-  return (
-    <div className="p-2 rounded-lg align-middle min-w-full overflow-auto mx-auto">
-      <div className="shadow overflow-hidden border-b rounded-lg">
-        <table className="min-w-full divide-y divide-black table-auto">
-          <thead className="bg-gray-50">{tableHeader}</thead>
-          <tbody className="bg-white divide-y divide-gray-200">
-            {courseRows}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
+  return <tbody data-testid="courses-table">{courseRows}</tbody>;
 };
+
 export default CourseRow;
