8e5d68a05ee0b97079589f0f687152684e236e17
rglasgow-deloitte <rglasgow@deloitte.com>
2021-05-12 16:47:57 -0400

    ECC-698: Adds test cases for CourseRow.js

---
+++ src/components/Dashboard/Courses/CourseRow/CourseRow.test.js
@@ -0,0 +1,215 @@
+import { render, act, screen } from "@testing-library/react";
+import { unmountComponentAtNode } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import CourseRow from "./CourseRow";
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+describe("CourseRow", () => {
+  test("does render", () => {
+    act(() => {
+      render(<CourseRow />, container);
+    });
+
+    expect(screen.getByTestId("courses-table")).toBeInTheDocument();
+  });
+
+  test("does render when passed data", () => {
+    act(() => {
+      const data = [
+        {
+          unique_record_identifier: "Record Identifer",
+          date_deleted: null,
+          date_inserted: "2021-04-28T14:46:33.629277Z",
+          date_transmitted: null,
+          metadata: {
+            Course: {
+              CourseURL: "https://www.testurl.com",
+              CourseCode: "TestCode123",
+              CourseLevel: "Intermediate",
+              CourseTitle: "Test Title",
+              DepartmentName: "Department Name",
+              CourseProviderName: "edX",
+              CoursePrerequisites: "Prerequisites",
+              CourseSubjectMatter: "Subject Matter",
+              CourseFullDescription: "Course Description",
+              EstimatedCompletionTime: 40,
+              CourseAdditionalInformation: "extra_description",
+            },
+            CourseInstance: {
+              EndDate: "2021-12-31T04:00:00Z",
+              Duration: 40,
+              CourseURL: "https://www.testurl.com",
+              StartDate: "2021-02-20T04:00:00Z",
+              Thumbnail: "https://www.testurl.com",
+              CourseCode: "course-v1:TsinghuaX+30240184.2x+1T2021",
+              InLanguage: "zh-cmn",
+              Instructor: "Deng",
+              CourseLevel: "Intermediate",
+              CourseTitle: "Test Title",
+              DeliveryMode: "Self",
+              InstructorBio: "Instructor Bio",
+              EnrollmentEndDate: "",
+              EnrollmentStartDate: "",
+              CourseFullDescription: "Full Course Description",
+              CourseLearningOutcome: "Course Learning Outcome",
+              CourseShortDescription: "Course Short Description",
+            },
+            Technical_Information: {
+              Thumbnail: "https://www.testurl.com",
+            },
+          },
+          metadata_hash: "123412312",
+          metadata_key: "TestCode123",
+          metadata_key_hash: "1234",
+          metadata_transmission_status: "Ready",
+          metadata_transmission_status_code: "",
+          provider_name: "edX",
+          record_status: "Active",
+          updated_by: "System",
+        },
+      ];
+
+      render(
+        <BrowserRouter>
+          <CourseRow data={data} />
+        </BrowserRouter>,
+        container
+      );
+    });
+
+    expect(screen.getByText("Test Title")).toBeInTheDocument();
+    expect(screen.getByText("CC: Record Identifer")).toBeInTheDocument();
+    expect(screen.getByText("Active")).toBeInTheDocument();
+  });
+
+  test("does render multiple components when data array is passed.", () => {
+    act(() => {
+      const data = [
+        {
+          unique_record_identifier: "Record Identifer",
+          date_deleted: null,
+          date_inserted: "2021-04-28T14:46:33.629277Z",
+          date_transmitted: null,
+          metadata: {
+            Course: {
+              CourseURL: "https://www.testurl.com",
+              CourseCode: "TestCode123",
+              CourseLevel: "Intermediate",
+              CourseTitle: "Test Title",
+              DepartmentName: "Department Name",
+              CourseProviderName: "edX",
+              CoursePrerequisites: "Prerequisites",
+              CourseSubjectMatter: "Subject Matter",
+              CourseFullDescription: "Course Description",
+              EstimatedCompletionTime: 40,
+              CourseAdditionalInformation: "extra_description",
+            },
+            CourseInstance: {
+              EndDate: "2021-12-31T04:00:00Z",
+              Duration: 40,
+              CourseURL: "https://www.testurl.com",
+              StartDate: "2021-02-20T04:00:00Z",
+              Thumbnail: "https://www.testurl.com",
+              CourseCode: "course-v1:TsinghuaX+30240184.2x+1T2021",
+              InLanguage: "zh-cmn",
+              Instructor: "Deng",
+              CourseLevel: "Intermediate",
+              CourseTitle: "Test Title",
+              DeliveryMode: "Self",
+              InstructorBio: "Instructor Bio",
+              EnrollmentEndDate: "",
+              EnrollmentStartDate: "",
+              CourseFullDescription: "Full Course Description",
+              CourseLearningOutcome: "Course Learning Outcome",
+              CourseShortDescription: "Course Short Description",
+            },
+            Technical_Information: {
+              Thumbnail: "https://www.testurl.com",
+            },
+          },
+          metadata_hash: "123412312",
+          metadata_key: "TestCode123",
+          metadata_key_hash: "1234",
+          metadata_transmission_status: "Ready",
+          metadata_transmission_status_code: "",
+          provider_name: "edX",
+          record_status: "Active",
+          updated_by: "System",
+        },
+        {
+          unique_record_identifier: "Record Identifer",
+          date_deleted: null,
+          date_inserted: "2021-04-28T14:46:33.629277Z",
+          date_transmitted: null,
+          metadata: {
+            Course: {
+              CourseURL: "https://www.testurl.com",
+              CourseCode: "TestCode123",
+              CourseLevel: "Intermediate",
+              CourseTitle: "Test Title",
+              DepartmentName: "Department Name",
+              CourseProviderName: "edX",
+              CoursePrerequisites: "Prerequisites",
+              CourseSubjectMatter: "Subject Matter",
+              CourseFullDescription: "Course Description",
+              EstimatedCompletionTime: 40,
+              CourseAdditionalInformation: "extra_description",
+            },
+            CourseInstance: {
+              EndDate: "2021-12-31T04:00:00Z",
+              Duration: 40,
+              CourseURL: "https://www.testurl.com",
+              StartDate: "2021-02-20T04:00:00Z",
+              Thumbnail: "https://www.testurl.com",
+              CourseCode: "course-v1:TsinghuaX+30240184.2x+1T2021",
+              InLanguage: "zh-cmn",
+              Instructor: "Deng",
+              CourseLevel: "Intermediate",
+              CourseTitle: "Test Title",
+              DeliveryMode: "Self",
+              InstructorBio: "Instructor Bio",
+              EnrollmentEndDate: "",
+              EnrollmentStartDate: "",
+              CourseFullDescription: "Full Course Description",
+              CourseLearningOutcome: "Course Learning Outcome",
+              CourseShortDescription: "Course Short Description",
+            },
+            Technical_Information: {
+              Thumbnail: "https://www.testurl.com",
+            },
+          },
+          metadata_hash: "123412312",
+          metadata_key: "TestCode123",
+          metadata_key_hash: "1234",
+          metadata_transmission_status: "Ready",
+          metadata_transmission_status_code: "",
+          provider_name: "edX",
+          record_status: "Active",
+          updated_by: "System",
+        },
+      ];
+
+      render(
+        <BrowserRouter>
+          <CourseRow data={data} />
+        </BrowserRouter>,
+        container
+      );
+    });
+    
+    expect(screen.getByTestId("courses-table").childElementCount).toBe(2);
+  });
+});
