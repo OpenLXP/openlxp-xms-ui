d27e92486eb1fc2029f663c897b0cdb5169c923f
rglasgow-deloitte <rglasgow@deloitte.com>
2021-05-12 13:41:57 -0400

    ECC-674: writes test case for CatalogCard.js and adds testid to components.

--- src/components/Dashboard/Catalogs/CatalogCard/CatalogCard.js
+++ src/components/Dashboard/Catalogs/CatalogCard/CatalogCard.js
@@ -6,9 +6,16 @@ const CatalogCard = (props) => {
   const img = props.img;
 
   return (
-    <NavLink to={{ pathname: "/dashboard/courses", state: title }} className="my-4">
+    <NavLink
+      to={{ pathname: "/dashboard/courses", state: title }}
+      className="my-4"
+      data-testid="nav-link">
       <div className="flex flex-row w-64 h-28 bg-gray-200 p-3 rounded-lg space-x-12 hover:h-32 hover:shadow-lg transition-shadow">
-        <img src={img} alt="" className="object-contain rounded-md" />
+        <img
+          src={img}
+          alt="catalog image"
+          className="object-contain rounded-md"
+        />
         <div className="font-sans font-thin self-center text-2xl">{title}</div>
       </div>
     </NavLink>
