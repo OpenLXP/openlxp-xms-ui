43f04cead48ddd534f7afb376da30144777676c3
rglasgow-deloitte <rglasgow@deloitte.com>
2021-05-13 14:08:50 -0400

    Refactors CourseList.js to handle the logic of rendering and makes CourseRow.js handle the rendering of individual rows.

---
+++ src/components/Dashboard/Courses/CourseList/CourseList.js
@@ -0,0 +1,23 @@
+import CourseHeader from "../CourseHeader/CourseHeader";
+import CourseRow from "../CourseRow/CourseRow";
+
+const CourseList = (props) => {
+  const courses = props.data || [];
+
+  // Creates the individual rows of the table.
+  const rows = courses.map((row, index) => {
+    if (Object.keys(row).length > 0)
+      return <CourseRow data={row} rowIndex={index} key={index} />;
+    return null;
+  });
+
+  return (
+    <table
+      className="min-w-full divide-y divide-black table-auto"
+      data-testid="course-table">
+      <CourseHeader />
+      <tbody data-testid="course-rows">{rows}</tbody>
+    </table>
+  );
+};
+export default CourseList;
--- src/components/Dashboard/Courses/CourseRow/CourseRow.js
+++ src/components/Dashboard/Courses/CourseRow/CourseRow.js
@@ -1,38 +1,42 @@
 import { NavLink } from "react-router-dom";
 
 const CourseRow = (props) => {
-  const data = props.data || [];
+  const data = props.data || undefined;
+  const index = props.rowIndex || 0;
 
-  const courseRows = data.map((row, index) => (
-    <tr
-      key={data.unique_record_identifier}
-      className={
-        index % 2 === 0
-          ? "bg-gray-50 hover:shadow-sm"
-          : "bg-white hover:shadow-lg"
-      }>
-      <td className="px-6 py-4 text-sm text-gray-900">
-        <div className="font-medium">{row.metadata.Course.CourseTitle}</div>
-        <div className="font-light">CC: {row.unique_record_identifier}</div>
-      </td>
+  if (data) {
+    return (
+      <tr
+        key={data.unique_record_identifier}
+        className={
+          index % 2 === 0
+            ? "bg-gray-50 hover:shadow-sm"
+            : "bg-white hover:shadow-lg"
+        }>
+        <td className="px-6 py-4 text-sm text-gray-900">
+          <div className="font-medium">{data.metadata.Course.CourseTitle}</div>
+          <div className="font-light">CC: {data.unique_record_identifier}</div>
+        </td>
 
-      <td className="px-6 py-4 text-sm text-gray-500 text-center">
-        <span className="px-2 inline-flex text-xs  leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-          {row.record_status}
-        </span>
-      </td>
+        <td className="px-6 py-4 text-sm text-gray-500 text-center">
+          <span className="px-2 inline-flex text-xs  leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+            {data.record_status}
+          </span>
+        </td>
 
-      <td className="px-6 py-4  text-center text-sm font-medium">
-        <NavLink
-          to={{ pathname: "/dashboard/course", state: row }}
-          className="text-blue hover:text-blue-light">
-          View
-        </NavLink>
-      </td>
-    </tr>
-  ));
+        <td className="px-6 py-4  text-center text-sm font-medium">
+          <NavLink
+            to={{ pathname: "/dashboard/course", state: data }}
+            className="text-blue hover:text-blue-light">
+            View
+          </NavLink>
+        </td>
+      </tr>
+    );
+  }
 
-  return <tbody data-testid="courses-table">{courseRows}</tbody>;
+  // Returns nothing if there is no data.
+  return null;
 };
 
 export default CourseRow;
