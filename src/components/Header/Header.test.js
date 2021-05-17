96866c5848070eaf0c94d3f8d4fb266be7089a4b
rglasgow-deloitte <rglasgow@deloitte.com>
2021-05-17 11:53:05 -0400

    Updates Header test to use memory router

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,6 +1,6 @@
 import { render, act, screen, fireEvent } from "@testing-library/react";
 import { unmountComponentAtNode } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, MemoryRouter, Route } from "react-router-dom";
 
 import Header from "./Header";
 
@@ -23,9 +23,9 @@ describe("Header", () => {
   test("does render", () => {
     act(() => {
       render(
-        <BrowserRouter>
+        <MemoryRouter>
           <Header />
-        </BrowserRouter>
+        </MemoryRouter>
       );
     });
 
@@ -33,5 +33,74 @@ describe("Header", () => {
     screen.getByText("Home");
     screen.getByText("Experience Management Service");
     screen.getByText("Department of Defense");
+  });
+
+  test("dose take user to home when clicking home", () => {
+    let testHistory, testLocation;
+    act(() => {
+      render(
+        <MemoryRouter initialEntries={["/my/initial/route"]}>
+          <Header />
+          <Route
+            path="/"
+            render={({ history, location }) => {
+              testHistory = history;
+              testLocation = location;
+            }}
+          />
+        </MemoryRouter>
+      );
+
+      const button = screen.getByText("Home");
+      fireEvent(button, new MouseEvent("click", { bubbles: true }));
+    });
+
+    expect(testLocation.pathname).toBe("/");
+  });
+
+  test("does take user to home when clicking Header Logo", () => {
+    let testHistory, testLocation;
+    act(() => {
+      render(
+        <MemoryRouter initialEntries={["/my/initial/route"]}>
+          <Header />
+          <Route
+            path="/"
+            render={({ history, location }) => {
+              testHistory = history;
+              testLocation = location;
+            }}
+          />
+        </MemoryRouter>
+      );
+
+      const button = screen.getByTestId("logo-button");
+      fireEvent(button, new MouseEvent("click", { bubbles: true }));
+    });
+
+    expect(testLocation.pathname).toBe("/");
+  });
+
+  test("dose take user to Dashboard when clicking Sign In", () => {
+    let testHistory, testLocation;
+    act(() => {
+      render(
+        <MemoryRouter initialEntries={["/my/initial/route"]}>
+          <Header />
+          <Route
+            path="/"
+            render={({ history, location }) => {
+              testHistory = history;
+              testLocation = location;
+            }}
+          />
+        </MemoryRouter>
+      );
+
+      const button = screen.getByText("Sign In");
+      fireEvent(button, new MouseEvent("click", { bubbles: true }));
+    });
+
+    expect(testLocation.pathname).toBe("/dashboard");
   });
 });
