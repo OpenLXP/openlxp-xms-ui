ebf252c9578415c0edd5663c59d8700cf592b467
rglasgow-deloitte <rglasgow@deloitte.com>
2021-05-17 13:41:55 -0400

    Refactors into its own file

--- src/components/Dashboard/Header/DashboardHeaderIcon.js
+++ src/components/Dashboard/Header/DashboardHeaderIcon.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-import icon from "../../resources/internal/dodLogo.png";
+import icon from "../../../resources/internal/dodLogo.png";
 
 const DashboardHeader = (props) =>{
     return(
