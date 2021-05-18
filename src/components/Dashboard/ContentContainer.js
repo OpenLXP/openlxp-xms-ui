eb882c7bb49ec6ac2d537b33830cdc84747b1c31
rglasgow-deloitte <rglasgow@deloitte.com>
2021-05-18 11:23:50 -0400

    Removed unnessary divs

--- src/components/Dashboard/ContentContainer.js
+++ src/components/Dashboard/ContentContainer.js
@@ -7,7 +7,11 @@ const ContentContainer = (props) => {
   // Routes must be added in reverse order
   const pathToComponents = [
     { path: "/dashboard/", component: CatalogsContainer, isExact: true },
-    { path: "/dashboard/:catalog/courses", component: CoursesContainer, isExact: false },
+    {
+      path: "/dashboard/:catalog/courses",
+      component: CoursesContainer,
+      isExact: false,
+    },
     {
       path: "/dashboard/:catalog/course/:id",
       component: CourseDataContainer,
@@ -17,23 +21,18 @@ const ContentContainer = (props) => {
 
   return (
     <main className="flex-1 relative overflow-y-auto focus:outline-none">
-      <div className="py-2">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-          {/* Replace with your content */}
-          <div className="py-4">
-            <Switch>
-              {pathToComponents.map((route, index) => (
-                <Route
-                  key={index}
-                  path={route.path}
-                  component={route.component}
-                  exact={route.isExact}
-                />
-              ))}
-            </Switch>
-          </div>
-          {/* /End replace */}
-        </div>
+      <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 md:px-8">
+        {/* Replace with your content */}
+        <Switch>
+          {pathToComponents.map((route, index) => (
+            <Route
+              key={index}
+              path={route.path}
+              component={route.component}
+              exact={route.isExact}
+            />
+          ))}
+        </Switch>
       </div>
     </main>
   );
