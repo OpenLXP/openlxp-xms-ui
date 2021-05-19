5b2efcf3f33e2cfe033b18f81cabf86ea0cc1b7c
rglasgow-deloitte <rglasgow@deloitte.com>
2021-05-19 11:25:20 -0400

    Removes Login import

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,6 +1,5 @@
 import { Switch, Route } from "react-router-dom";
 import Header from "../components/Header/Header";
-import Login from "../components/Login/Login";
 import WelcomeScreen from "../components/WelcomeScreen/WelcomeScreen";
 import Footer from "../components/Footer/Footer";
 const MainPage = () => {
