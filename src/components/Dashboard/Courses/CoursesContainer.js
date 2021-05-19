541b9d2d46560060735f13bfec42172a671ae4d1
rglasgow-deloitte <rglasgow@deloitte.com>
2021-05-19 09:49:47 -0400

    Optimizes imports

--- src/components/Dashboard/Courses/CoursesContainer.js
+++ src/components/Dashboard/Courses/CoursesContainer.js
@@ -1,6 +1,6 @@
-import axios from "axios";
 import React, { useState, useEffect } from "react";
-import { useLocation, NavLink } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 import CourseList from "./CourseList/CourseList";
 
 const Courses = (props) => {
@@ -42,24 +42,20 @@ const Courses = (props) => {
       });
   }, [catalog_courses_api_url]);
 
-  let content = null;
+  let table = null;
 
   if (courseData.isLoading) {
-    content = <div>Loading...</div>;
+    table = <div>Loading...</div>;
   } else if (courseData.courses && !courseData.isLoading) {
-    content = <CourseList data={courseData.courses} />;
+    table = <CourseList data={courseData.courses} />;
   } else {
-    content = (
-      <div>Error loading courses. Please contact an administrator.</div>
-    );
+    table = <div>Error loading courses. Please contact an administrator.</div>;
   }
 
   return (
     <div className="rounded-lg align-middle min-w-full overflow-auto mx-auto">
       <h2 className="text-2xl my-8">Course List</h2>
-      <div className="shadow overflow-hidden border-b rounded-lg">
-        {content}
-      </div>
+      <div className="shadow overflow-hidden border-b rounded-lg">{table}</div>
     </div>
   );
 };
