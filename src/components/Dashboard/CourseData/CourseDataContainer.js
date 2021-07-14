20a008660cab5de612db3f4c276b16aa8898ae76
rglasgow-deloitte <rglasgow@deloitte.com>
2021-07-14 15:59:10 -0400

    Updates the styling of the Course Data Editor

--- src/components/Dashboard/CourseData/CourseDataContainer.js
+++ src/components/Dashboard/CourseData/CourseDataContainer.js
@@ -8,6 +8,7 @@ const CourseData = (props) => {
   // Gets the location data for state
   const location = useLocation();
   const [courseData, setCourseData] = useState(location.state);
+  const [isEditing, setEditing] = useState(false);
 
   // state of the prepared data.
   const [preparedData, setPreparedData] = useState({});
@@ -117,8 +118,13 @@ const CourseData = (props) => {
     updatePreparedDataValue(event.target.value, pathToData);
   };
 
-  const handleSubmit = () => {
-    setCourseData(prepareDataForSubmit());
+  const handleSubmit = (event) => {
+    if (isEditing) {
+      setCourseData(prepareDataForSubmit());
+    }
+    setEditing(!isEditing);
+
+    // API call
   };
 
   // creates the html to render
@@ -126,22 +132,32 @@ const CourseData = (props) => {
     // Each sections data
     const sectionData = preparedData[sectionKey].data;
     return (
-      <div>
+      <div className="px-4 ">
         <div className="font-semibold text-2xl">{sectionKey}</div>
-        <div>
+        <div className="">
           {Object.keys(sectionData).map((key) => {
             return (
-              <div className="flex flex-row">
-                <label className="py-2 pr-2">
-                  {sectionData[key].pathToData.split(".")[2]}:
+              <div className="flex flex-col my-3">
+                <label className="relative inline-flex max-w-max items-center space-x-2 px-4 py-2 border-t border-l border-r border-gray-300 text-sm font-medium rounded-t-md text-gray-700 bg-gray-50 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 select-none">
+                  {sectionData[key].pathToData.split(".")[2]}
                 </label>
-                <input
+                <textarea
+                  disabled={!isEditing}
                   onChange={(event) =>
                     handleEdit(event, sectionData[key].pathToData)
                   }
+                  rows={Math.max(
+                    Math.floor(sectionData[key].value.length / 50) - 1,
+                    1
+                  )}
                   type="text"
-                  placeholder={sectionData[key].value}
-                  className="w-full"
+                  placeholder={sectionData[key].value.toString().trim()}
+                  className="w-full relative inline-flex items-center space-x-2 px-4 py-2 border border-gray-300 text-sm font-medium rounded-b-md rounded-tr-md hover:bg-blue-light hover:bg-opacity-5 focus:outline-none focus:ring-1 focus:ring--blue-light focus:border-blue-light focus:shadow-md text-gray-400 focus:text-gray-700"
+                  onFocus={(event) => {
+                    event.target.value = sectionData[key].value
+                      .toString()
+                      .trim();
+                  }}
                 />
               </div>
             );
@@ -159,14 +175,16 @@ const CourseData = (props) => {
   }, [courseData]);
 
   return (
-    <div className="bg-white shadow overflow-hidden rounded-md">
+    <div className="bg-white shadow overflow-hidden rounded-md pb-8">
       <CourseHeader title={courseTitle} status={courseData.record_status} />
-      <div
-        className="h-24 w-36 bg-blue-500"
-        onClick={() => {
-          handleSubmit();
-        }}
-      ></div>
+      <div className="flex flex-row justify-end px-4">
+        <div
+          className="px-3 py-1 border rounded-md hover:bg-blue-light hover:text-white cursor-pointer select-none"
+          onClick={(event) => handleSubmit()}
+        >
+          {isEditing ? "Update" : "Edit"}
+        </div>
+      </div>
       <div>{renderSections}</div>
     </div>
   );
