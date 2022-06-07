28235b73daeb8f413e7edd2dc85a570ba208a88e
schittlur <78377530+schittlur@users.noreply.github.com>
2022-06-07 14:47:23 -0400

    catalog image to display backend image

--- src/components/Dashboard/Catalogs/CatalogList/CatalogList.js
+++ src/components/Dashboard/Catalogs/CatalogList/CatalogList.js
@@ -1,9 +1,34 @@
 import CatalogCard from "../CatalogCard/CatalogCard";
 import icon from "../../../../images/placeholder.jpg";
+import { configUrl, host } from "../../../../config/endpoints";
+import { useState, useEffect } from "react";
+import { axiosInstance } from "../../../../config/axiosInstance";
 
 const CatalogsList = ({catalogs}) => {
 
+  const [config, setConfig] = useState(null);
+  useEffect(() => {
+    axiosInstance.get(configUrl)
+      .then((response) => {
+        console.log(response);
+        setConfig({
+          config: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  console.log(catalogs);
+
+
   const content = catalogs?.map((catalog) => {
+    console.log(config?.config?.find(data => data.name === catalog));
+    let configData = config?.config?.find(data => data.name === catalog);
+    if(configData){
+      console.log("here");
+      return <CatalogCard title={catalog} img={`${host}${configData.image.substring(1, configData.image.length)}`} key={catalog} />;
+    }
     return <CatalogCard title={catalog} img={icon} key={catalog} />;
   });
 
--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -9,3 +9,6 @@ export const catalog_courses_url = `${host}${api}catalogs/`
 //user login/register
 export const login_url = `${host}${api}auth/login`
 export const register_url = `${host}${api}auth/register`
+
+//configs
+export const configUrl = `${host}${api}config/catalogs/`
