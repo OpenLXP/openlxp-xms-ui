626fe35290b342e5b502270bc9bf6b85fcaeef85
schittlur <78377530+schittlur@users.noreply.github.com>
2022-06-09 16:47:33 -0400

    Origin/navbar reformat (#23)
    * Header reformat
    
    * Fixes failing tests

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useAuth } from "../../context/authContext";
 
 // Local imports
 import icon from "../../resources/internal/dodLogo.png";
@@ -13,6 +14,11 @@ const Header = () => {
       route: "/",
       testId: "home-btn",
     },
+    {
+      name: "Catalogs", 
+      route: "/dashboard",
+      testID: "catalog-btn"
+    },
   ];
 
   // to be replaced from the admin console
@@ -22,27 +28,41 @@ const Header = () => {
     img: icon,
   };
 
+  const {user, logout} = useAuth();
+
   return (
     <header className="bg-gradient-to-t from-blue-medium to-blue text-white">
-      <div className="w-10/12 mx-auto h-auto pt-5">
+      <div className="w-10/12 mx-auto h-auto mt-2 pt-2">
         <HeaderLogo
           title={logo.title}
           subtitle={logo.subtitle}
           img={logo.img}
         />
-        <div className=" border-t flex flex-row justify-between mt-4 mx-2">
+        <div className=" border-t flex flex-row justify-between mt-2 mx-2">
           <NavBar navButtons={navButtons} />
           <div className="flex flex-row justify-end">
-            <div className="hover:bg-blue-light rounded-t-lg md:text-lg mt-2 p-2 mx-2">
-              <NavLink to={"/login"} className="hover:bg-blue-light p-0 mt-2">
-                Sign In
-              </NavLink>
-            </div>
-            <div className=" hover:bg-blue-light rounded-t-lg md:text-lg mt-2 p-2 mx-2">
-              <NavLink to={"/register"} className="hover:bg-blue-light p-0 mt-2">
-                Register
-              </NavLink>
-            </div>
+            {!user ? (
+              <div className="flex flex-row">
+                <div className="hover:bg-blue-light rounded-t-lg md:text-lg mt-2 p-2 mx-2">
+                  <NavLink to={"/login"} className="hover:bg-blue-light p-0 mt-2">
+                    Sign In
+                  </NavLink>
+                </div>
+                <div className=" hover:bg-blue-light rounded-t-lg md:text-lg mt-2 p-2 mx-2">
+                  <NavLink to={"/register"} className="hover:bg-blue-light p-0 mt-2">
+                    Register
+                  </NavLink>
+                </div>
+              </div>
+            ) : (
+              <div>
+                <span className="px-2 text-white">
+                  {user?.user?.first_name}&nbsp;{user?.user?.last_name}
+                </span>
+                <button className="hover:bg-blue-light rounded-t-lg md:text-lg mt-2 p-2 mx-2" 
+                onClick={()=>{logout()}}>Logout </button>
+              </div>
+            )}
           </div>
         </div>
       </div>
--- src/components/WelcomeScreen/WelcomeScreen.js
+++ src/components/WelcomeScreen/WelcomeScreen.js
@@ -1,17 +1,17 @@
 const WelcomeScreen = () => {
   // custom text from the admin panel
   let welcome = {
-    title: "Welcome!",
-    subtitle: "DOD - Experience Management Service",
+    // title: "Welcome!",
+    subtitle: "About Experience Management Service",
     message: [
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui ipsum molestiae illum cumque aliquid dolorem veniam iusto facere dolorum architecto maiores recusandae ipsa nihil sapiente eius nostrum, mollitia voluptatum error quis quos deleniti molestias enim. Provident laborum, inventore unde error quas, praesentium excepturi ipsa molestiae quasi eos voluptatem omnis eaque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui ipsum molestiae illum cumque aliquid dolorem veniam iusto facere dolorum architecto maiores recusandae ipsa nihil sapiente eius nostrum, mollitia voluptatum error quis quos deleniti molestias enim. Provident laborum, inventore unde error quas, praesentium excepturi ipsa molestiae quasi eos voluptatem omnis eaque! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui ipsum molestiae illum cumque aliquid dolorem veniam iusto facere dolorum architecto maiores recusandae ipsa nihil sapiente eius nostrum, mollitia voluptatum error quis quos deleniti molestias enim. Provident laborum, inventore unde error quas, praesentium excepturi ipsa molestiae quasi eos voluptatem omnis eaque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui ipsum molestiae illum cumque aliquid dolorem veniam iusto facere dolorum architecto maiores recusandae ipsa nihil sapiente eius nostrum, mollitia voluptatum error quis quos deleniti molestias enim. Provident laborum, inventore unde error quas, praesentium excepturi ipsa molestiae quasi eos voluptatem omnis eaque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui ipsum molestiae illum cumque aliquid dolorem veniam iusto facere dolorum architecto maiores recusandae ipsa nihil sapiente eius nostrum, mollitia voluptatum error quis quos deleniti molestias enim. Provident laborum, inventore unde error quas, praesentium excepturi ipsa molestiae quasi eos voluptatem omnis eaque! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui ipsum molestiae illum cumque aliquid dolorem veniam iusto facere dolorum architecto maiores recusandae ipsa nihil sapiente eius nostrum, mollitia voluptatum error quis quos deleniti molestias enim. Provident laborum, inventore unde error quas, praesentium excepturi ipsa molestiae quasi eos voluptatem omnis eaque!",
+      "The Experience Management Service is the human-facing application enabling an Experience Owner or Experience Manager to modify or augment a learning metadata record ingested by the Experience Index Service (XIS).  XMS is a user interface facilitating modification and augmentation of records by learning experience owners and managers. This web application enables experience owners/managers to modify/augment experience metadata (i.e., the admin portal)"
     ],
   };
 
   return (
     <>
       <main className="font-sans">
-        <h1 className="text-5xl my-8">{welcome.title}</h1>
+        {/* <h1 className="text-5xl my-8">{welcome.title}</h1> */}
         <h3 className="text-3xl mb-5">{welcome.subtitle}</h3>
         {welcome.message.map((message, index) => {
           return (
