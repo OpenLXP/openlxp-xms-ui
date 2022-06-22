3dde0c36073a0b49ed38007d484b220ead373bb7
schittlur <78377530+schittlur@users.noreply.github.com>
2022-06-22 12:29:39 -0400

    Origin/unit testing (#24)
    * Course List header test
    
    * Login and Register page redesign
    
    * Help page
    
    * align content
    
    * sticky title and edit/update buttons
    
    * pagination format css
    
    * Sign in/sign out button css

--- src/components/WelcomeScreen/WelcomeScreen.js
+++ src/components/WelcomeScreen/WelcomeScreen.js
@@ -4,7 +4,8 @@ const WelcomeScreen = () => {
     // title: "Welcome!",
     subtitle: "About Experience Management Service",
     message: [
-      "The Experience Management Service is the human-facing application enabling an Experience Owner or Experience Manager to modify or augment a learning metadata record ingested by the Experience Index Service (XIS).  XMS is a user interface facilitating modification and augmentation of records by learning experience owners and managers. This web application enables experience owners/managers to modify/augment experience metadata (i.e., the admin portal)"
+      "The Experience Management Service is the human-facing application enabling an Experience Owner or Experience Manager to modify or augment a learning metadata record ingested by the Experience Index Service (XIS).  XMS is a user interface facilitating modification and augmentation of records by learning experience owners and managers. This web application enables experience owners/managers to modify/augment experience metadata (i.e., the admin portal).",
+      "Please navigate to the Sign in or Register page to get started!"
     ],
   };
 
@@ -20,6 +21,7 @@ const WelcomeScreen = () => {
             </div>
           );
         })}
+
       </main>
     </>
   );
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,6 +2,8 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useAuth } from '../context/authContext';
+import logo from "../resources/internal/dodLogo.png"
+
 
 export default function Register() {
     const {user, register} = useAuth();
@@ -31,7 +33,18 @@ export default function Register() {
     }
     return(
         <div className='w-1/3 mx-auto p-8 rounded flex flex-col justify-center m-10'> 
-            <h1 className="my-2 text-2xl font-bold">Register</h1>
+            <img src={logo} alt="Dod Logo" width={150} className='mx-auto' />
+            <h1 className="my-2 mx-auto text-2xl font-bold"> Create your account </h1>
+
+            <span className="mx-auto">
+                or &nbsp;
+                <button
+                    id={'create-account-button'}
+                    className='text-blue-400 hover:underline hover:text-blue-500 cursor-pointer transition-all duration-150 ease-in-out'
+                    onClick={() => history.push("/login")}>
+                    Sign in to your Account
+                </button>
+            </span>
             <form className="p-2 align-center" onSubmit={handleSubmit} onChange={handleChange}>
                 <input 
                     className='shadow focus:shadow-md rounded-md p-2 w-full border border-gray-200 text-gray-700 focus:ring-2 outline-none transition-all  duration-200'
@@ -48,9 +61,11 @@ export default function Register() {
                 <input 
                     className='mt-2 shadow focus:shadow-md rounded-md p-2 w-full border border-gray-200 text-gray-700 focus:ring-2 outline-none transition-all  duration-200'
                     type="password" name="confirmPassword" placeholder="Confirm Password"/>
+                <div className='mx-auto justify-center'>
                 <button 
-                    className='mt-4 mx-auto max-w-max items-center inline-flex gap-2 bg-blue-200 rounded-md hover:shadow-md hover:bg-blue hover:text-white px-4 py-2 transform transition-all duration-75 ease-in-out border-bg-blue border-2 outline-none focus:ring-2 ring-blue-400'
+                    className='mt-4 mx-auto gap-2 bg-blue-200 rounded-md hover:shadow-md hover:bg-blue hover:text-white px-4 py-2 transform transition-all duration-75 ease-in-out border-bg-blue border-2 outline-none focus:ring-2 ring-blue-400'
                     type="submit">Register</button>
+                </div>
             </form>
         </div>
     )
