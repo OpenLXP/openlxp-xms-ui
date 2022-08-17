9132ab44c3443d1e23bbfad650d047ba4e1cede0
schittlur <78377530+schittlur@users.noreply.github.com>
2022-08-17 12:58:27 -0400

    support page content

--- src/pages/Support.js
+++ src/pages/Support.js
@@ -3,24 +3,41 @@ import { ChevronDownIcon } from '@heroicons/react/outline';
 
 const Support = () => {
 
-    const content = [
-        {   title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam gravida, neque id scelerisque commodo, nisi metus tristique nunc, ut sodales risus quam non lacus. Nulla dictum sit amet justo a accumsan. Sed ac augue vitae leo consectetur lacinia. Ut in est eu nisi pretium sodales et consectetur neque."
-        },
-        {   title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam gravida, neque id scelerisque commodo, nisi metus tristique nunc, ut sodales risus quam non lacus. ",
-            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam gravida, neque id scelerisque commodo, nisi metus tristique nunc, ut sodales risus quam non lacus. Nulla dictum sit amet justo a accumsan. Sed ac augue vitae leo consectetur lacinia. Ut in est eu nisi pretium sodales et consectetur neque. Proin lectus turpis, interdum et tellus ut, suscipit sollicitudin ex. Donec vel nibh vitae tortor venenatis egestas at sit amet justo. Praesent in nulla velit. Aenean sollicitudin efficitur ex, sed vehicula neque aliquam vitae. In congue, turpis ac dictum eleifend, est eros tempus lorem, vel tincidunt urna enim ut est. Pellentesque mi sem, varius in arcu eleifend, sodales vestibulum velit. Sed aliquam molestie mauris, id vestibulum orci pharetra quis. Donec ex elit, auctor ac euismod eu, pretium ac massa. Mauris id mattis elit. Integer dictum felis mauris, ac sagittis tellus fermentum ut. Proin aliquet sem sed venenatis consequat. Nulla facilisis fermentum vulputate."
-        },
-        {   title: "test",
-            answer: "test ans"
-        },
-        ]
+    const aboutContent = [
+    {   title: "What is ECC?",
+        answer: "The Enterprise Course Catalog (ECC) is a learning and experience discovery service. Course catalogs are stored in a centralized location for consistent transport. ECC aggregates metadata describing learning experiences from internal and external sources. Course catalogs across organizations are now easier to manage, join and identify ownership. This improves the ability to find courses across all of Department of Defense’s organizations."
+    },
+    {   title: "What is XMS?",
+        answer: "ECC’s Experience Management Service (XMS) is your one stop shop to change, modify, delete courses being presented on ECC’s Experience Discovery Service (XDS)."
+    },
+    ]
+    const navigateContent = [
+    {   title: "How to find catalogs?",
+        answer: "In XMS click on the top button which says “Catalog” to search for courses."
+    },
+    {   title: "How to search for course?",
+        answer: "In XMS select and click “Catalog”. Then click on a provider to view courses offered from that specific provider. After clicking on a provider, you should be presented a search field, at the top of the page, where you can type the name of your course then filter the results based on your search term."
+    },
+    {   title: "How to change/edit fields?",
+        answer: "In XMS select and click “Catalog”. You will be presented a series of providers to choose from. Once you choose a provider you will see courses populated. Click onto a course to view the course’s details. On this page you will have access to an “edit” button on the top right corner. Click on this button to modify the data fields being presented. Be sure to save your changes by clicking on the “update” button located at the top right of the page."
+    },
+    {   title: "How to add fields?",
+        answer: "In XMS select and click “Catalog”. You will be presented a series of providers to choose from. Once you choose a provider you will see courses populated. Click onto a course to view the course’s details. Click the “Edit” button. Then type in the name of the field you wish to add in “Key Name” and text into the field labeled “Value”. Click “Update” on the top right of the page to save your changes."
+    },
+    {   title: "How to delete fields?",
+        answer: "In XMS select and click “Catalog”. You will be presented a series of providers to choose from. Once you choose a provider you will see courses populated. Click onto a course to view the course’s details. Click the “Edit” button. Then click ‘Delete’ button associated with the key value pair you wish to delete. Click “Update” on the top right of the page to save your changes."
+    },
+    {   title: "Need more support?",
+        answer: "Contact your local system administrator."
+    },
+    ]
     
     const panelCode = (content) =>
         content.map((question, index) => {
             return(
                 <Disclosure key={index}>
                 {({ open }) => (
-                <div className='p-2 hover:ring-4 hover:ring-blue hover:bg-gray-50'>
+                <div className='p-2 hover:bg-gray-200 hover:rounded-lg'>
                     <Disclosure.Button className="flex items-center rounded-lg justify-between text-left w-full p-5 font-medium border bg-blue text-white border-gray-300 dark:focus:ring-gray-800 hover:opacity-90 hover:shadow ">
                         {question.title}
                         <ChevronDownIcon className={`w-6 h-6 ${open ? "transform rotate-180" : ""} `} />
@@ -34,7 +51,7 @@ const Support = () => {
                         leaveFrom="transform scale-100 opacity-100"
                         leaveTo="transform scale-95 opacity-0"
                     >
-                    <Disclosure.Panel className="p-5 rounded-lg border border-t-0 ml-2 border-gray-200 dark:border-gray-700 focus:ring-4 focus:ring-gray-200 focus:bg-gray-50">
+                    <Disclosure.Panel className="p-5 rounded-lg border border-t-0 ml-2 border-gray-300 dark:border-gray-700 focus:ring-4 focus:ring-gray-200 focus:bg-gray-50">
                         {question.answer}
                     </Disclosure.Panel>
                     </Transition>
@@ -47,10 +64,10 @@ const Support = () => {
         // <main className="font-sans">
             <div className='pb-20'>
                 <h1 className='pb-4 border-b mb-8 text-3xl font-semibold'>Support</h1>
-                <h2 className='text-xl font-semibold pb-2'>About</h2>
-                {panelCode(content)}
-                <h2 className='text-xl font-semibold pb-2 mt-4'>Purpose</h2>
-                {panelCode(content)}
+                <h2 className='text-2xl font-semibold pb-2'>About</h2>
+                {panelCode(aboutContent)}
+                <h2 className='text-xl font-semibold pb-2 mt-4'>Navigating XMS</h2>
+                {panelCode(navigateContent)}
             </div>
         // </main>
     );
