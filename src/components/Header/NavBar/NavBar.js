0ebabd60ca3d39978eb3358b6c76ba1ed1cc47b8
schittlur <78377530+schittlur@users.noreply.github.com>
2022-08-19 16:21:47 -0400

    css fix for header (#32)

--- src/components/Header/NavBar/NavBar.js
+++ src/components/Header/NavBar/NavBar.js
@@ -23,7 +23,7 @@ const NavBar = (props) => {
         if (user) {
           return (
             <div
-              className="p-2 mx-2 hover:bg-blue-light lg:text-lg rounded-t-lg md:text-lg"
+              className="p-2 mx-2 hover:bg-blue-light lg:text-md rounded-t-lg md:text-md"
               key={index}>
               <NavLink to={button.route} data-testid={button.testId}>
                 {button.name}
