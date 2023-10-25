fc0f76fc6756e3ce6dfc7df9f3cb1bcbae8cd1c0
LFRMadrid <lreyesmadrid@deloitte.com>
2023-10-25 16:29:44 -0400

    Update craco.config.js
    some security headers where added and still pending csp ones

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,6 @@
 // craco.config.js
 module.exports = {
-  devServer: (cfg) => { cfg.headers = { 'X-Frame-Options': 'Deny' }; return cfg},
+  devServer: (cfg) => { cfg.headers = { 'X-Frame-Options': 'Deny', 'X-Content-Type-Options': 'nosniff', 'Strict-Transport-Security' : 'max-age=63072000; includeSubDomains; preload' }; return cfg},
   style: {
     postcss: {
       plugins: [require("tailwindcss"), require("autoprefixer")],
