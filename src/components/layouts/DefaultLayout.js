9a01a9ddb038fff27d63b71237efc3a15794b039
schittlur <78377530+schittlur@users.noreply.github.com>
2023-11-17 15:10:32 -0500

    placing footer at bottom of screen

--- src/components/layouts/DefaultLayout.js
+++ src/components/layouts/DefaultLayout.js
@@ -1,19 +1,18 @@
 'use strict';
 
-// import { useConfig } from '../../hooks/useConfig';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 
 export default function DefaultLayout({ children, footerLocation }) {
-//   useConfig();
   return (
     <div className={'relative custom-scroll min-h-screen'}>
       <Header />
-      {/* <div className='max-w-7xl mx-auto px-4 sm:px-4 lg:px-8'> */}
       <div className='w-9/12 mx-auto px-4 sm:px-6 md:px-8'> 
         {children}
       </div>
-      <Footer location={footerLocation} />
+      <div className='absolute bottom-0 w-full'>
+        <Footer />
+      </div>
     </div>
   );
 }
