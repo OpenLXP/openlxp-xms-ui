4a715f8b4e8e9ec2d42617f92729c698f5052ffd
schittlur <78377530+schittlur@users.noreply.github.com>
2024-02-07 16:52:48 -0500

    course Metadata test added

---
+++ src/__tests__/pages/Dashboard/Catalogs/[catalogTitle]/[courseMetadataKey].test.js
@@ -0,0 +1,84 @@
+'use strict';
+
+import { fireEvent, act, screen, render } from "@testing-library/react";
+import { unmountComponentAtNode } from "react-dom";
+import axios from "axios";
+import CourseDataContainerV2 from "../../../../../pages/dashboard/[catalogTitle]/[courseMetadataKey]";
+import { MemoryRouter } from "react-router-dom";
+import MockAxios from 'jest-mock-axios';
+import { useAuth } from "../../../../../context/authContext"
+// mocking axios
+// jest.mock("axios");
+jest.mock('../../../../../context/authContext', () => ({
+  useAuth: jest.fn(),
+}));
+
+let container = null;
+beforeEach(() => {
+  useAuth.mockImplementation(() => ({
+    user: { 
+      user: {
+        id: '1',
+        first_name: 'Test',
+        last_name: 'User',
+        email: 'test@test.com',
+    }},
+  }));
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  MockAxios.reset();
+  container = null;
+});
+
+describe("CourseDataContainerV2", () => {
+  it("does render", async () => {
+    await act(async () => {
+      MockAxios.get.mockImplementation(() => Promise.resolve({ data: {experiences: [{}]} }));
+
+      render(
+        <MemoryRouter>
+          <CourseDataContainerV2 />
+        </MemoryRouter>,
+        container
+      );
+    });
+    screen.getByText("Edit");
+  });
+
+  it("does render edit", async () => {
+    await act(async () => {
+      MockAxios.get.mockImplementation(() => {
+        return Promise.resolve({ data: {experiences: [{}]} });
+      });
+      
+      await render(
+        <MemoryRouter>
+          <CourseDataContainerV2 />
+        </MemoryRouter>,
+        container
+      );
+    });
+
+    // screen.getByPlaceholderText("Search");
+    // screen.getByText("Clear Search");
+    // screen.getByText("Next");
+    // screen.getByText("Previous");
+    act(() => {
+        const button = screen.getByText(/Edit/i);
+        fireEvent(button, new MouseEvent("click", { bubbles: true }));
+    });
+
+    act(() => {
+        const button = screen.getByText(/Update/i);
+        fireEvent(button, new MouseEvent("click", { bubbles: true }));
+    });
+    
+  });
+
+});
